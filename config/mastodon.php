ce71f22506422db3cabc21f305c31324d6455627
Tii <mail@tii.one>
2024-05-18 10:50:57 +0200

    Cleaned up config variables

--- config/mastodon.php
+++ config/mastodon.php
@@ -4,10 +4,6 @@ return [
 
     'instance_url' => env('MASTODON_INSTANCE_URL'),
 
-    'client_id' => env('MASTODON_CLIENT_ID'),
-
-    'client_secret' => env('MASTODON_CLIENT_SECRET'),
-
     'access_token' => env('MASTODON_ACCESS_TOKEN'),
 
 ];
